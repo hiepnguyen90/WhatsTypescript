30a97f68bfd153b034d048799c2e4af1ae7b8b1e
hiepnguyen90 <johnhiepnguyen1@gmail.com>
2023-04-15 20:45:30 -0400

    yolo

--- index.ts
+++ index.ts
@@ -1,15 +1,26 @@
-// Import stylesheets
-import './style.css';
+const form: HTMLFormElement = document.querySelector('#defineform')!;
+const list: HTMLUListElement = document.querySelector('.list-unstyled')!;
+const header: HTMLHeadingElement = document.querySelector('h1')!;
 
+form.onsubmit = async (event) => {
+  event.preventDefault();
 
-const form: HTMLFormElement = document.querySelector('#defineform');
+  const formData = new FormData(form); // forma data from form
+  const text = formData.get('defineword') as string; // get the word user input
 
+  try {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`);
+    const data = await response.json();
 
-form.onsubmit = () => {
-  const formData = new FormData(form);
+    header.innerText = text;
 
-  console.log(formData);
-  const text = formData.get('defineword') as string;
-  console.log(text);
-  return false; // prevent reload
+    list.innerHTML = '';
+    data[0].meanings.forEach((meaning: any) => {
+      const li = document.createElement('li');
+      li.innerText = `${meaning.partOfSpeech} - ${meaning.definitions[0].definition}`;
+      list.appendChild(li);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
